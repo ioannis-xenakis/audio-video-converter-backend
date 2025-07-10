1f698378fa7dd671ba5f5c13ec7222c65940d88e
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-07-10 17:53:31 +0300

    Create new java project

---
+++ src/main/java/com/john_xenakis/audio_video_converter_backend/AudioVideoConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.john_xenakis.audio_video_converter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioVideoConverterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioVideoConverterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/john_xenakis/audio_video_converter_backend/AudioVideoConverterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john_xenakis.audio_video_converter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioVideoConverterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
