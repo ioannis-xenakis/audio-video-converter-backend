7a886f1fce6dca6cbf8b262d7b60a9440c6e94ca
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-07-27 20:08:34 +0300

    Change relative path to absolute path
    Change missing relative path, to different absolute path.
    
    Path was under directory that didnt exist, with this commit, path is absolute and is different.

--- upload-service/src/main/java/com/johnxenakis/converter/upload/service/FileStorageService.java
+++ upload-service/src/main/java/com/johnxenakis/converter/upload/service/FileStorageService.java
@@ -1,5 +1,6 @@
 package com.johnxenakis.converter.upload.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,15 +10,17 @@ import java.util.UUID;
 
 @Service
 public class FileStorageService {
-    private static final String UPLOAD_DIR = "uploads/";
+    @Value("${upload.dir}")
+    private String uploadDir;
 
     public String store(MultipartFile file) {
         try {
-            Files.createDirectories(Paths.get(UPLOAD_DIR));
+            Path uploadPath = Paths.get(uploadDir);
+            Files.createDirectories(uploadPath);
 
             String ext = getExtension(file.getOriginalFilename());
             String fileId = UUID.randomUUID().toString() + (ext.isEmpty() ? "" : "." + ext);
-            Path path = Paths.get(UPLOAD_DIR, fileId);
+            Path path = uploadPath.resolve(fileId);
 
             file.transferTo(path.toFile());
 
