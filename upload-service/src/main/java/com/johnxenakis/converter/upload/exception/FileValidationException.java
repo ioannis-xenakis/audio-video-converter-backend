dacb33a1fa42846e3c53bea601f1cbcdb01b298d
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-07-31 21:24:06 +0300

    Add file name and reason strings to file validation exception
    Add fileName and reason strings with getters functions in FileValidationException.java.

--- upload-service/src/main/java/com/johnxenakis/converter/upload/exception/FileValidationException.java
+++ upload-service/src/main/java/com/johnxenakis/converter/upload/exception/FileValidationException.java
@@ -1,7 +1,20 @@
 package com.johnxenakis.converter.upload.exception;
 
 public class FileValidationException extends RuntimeException {
-    public FileValidationException(String message) {
-        super(message);
+    private final String fileName;
+    private final String reason;
+
+    public FileValidationException(String fileName, String reason) {
+        super(reason);
+        this.fileName = fileName;
+        this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getFileName() {
+        return fileName;
     }
 }
