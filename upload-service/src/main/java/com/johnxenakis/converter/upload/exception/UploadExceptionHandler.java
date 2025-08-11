4a58a4b44aac019fbbdfdd452a4bab40afd29fdd
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-08-11 19:21:41 +0300

    Add json response for big file sizes
    Return json response for max upload size exceeded exception.
    
    Resolves CU-86c4w9nq4[complete]

--- upload-service/src/main/java/com/johnxenakis/converter/upload/exception/UploadExceptionHandler.java
+++ upload-service/src/main/java/com/johnxenakis/converter/upload/exception/UploadExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.johnxenakis.converter.upload.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,5 +17,13 @@ public class UploadExceptionHandler {
         errorResponse.put("file", ex.getFileName());
         errorResponse.put("error", ex.getReason());
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxSizeExceeded(MaxUploadSizeExceededException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "File is too large");
+        errorResponse.put("message", "File cannot exceed file size more than 200MB");
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(errorResponse);
     }
 }
