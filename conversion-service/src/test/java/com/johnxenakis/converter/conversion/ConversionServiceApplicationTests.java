1499f8ee4693f3bf6fa3d1c5027e96cca9f52771
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-08-18 19:42:18 +0300

    Create new conversion service
    Resolves CU-86c4nayeh[complete]

---
+++ conversion-service/src/test/java/com/johnxenakis/converter/conversion/ConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnxenakis.converter.conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
