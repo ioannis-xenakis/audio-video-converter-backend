f5e1726490c5ca52cb50aa6d30e04f2b329953fa
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-08-29 13:23:36 +0300

    Add setters and getters for audio and video codecs
    Add audio video codecs setters and getters in ConversionRequest.java.

--- conversion-service/src/main/java/com/johnxenakis/converter/conversion/model/ConversionRequest.java
+++ conversion-service/src/main/java/com/johnxenakis/converter/conversion/model/ConversionRequest.java
@@ -4,14 +4,18 @@ public class ConversionRequest {
     private String inputPath;
     private String outputPath;
     private String format;
+    private String audioCodec;
+    private String videoCodec;
 
     public ConversionRequest() {
     }
 
-    public ConversionRequest(String inputPath, String outputPath, String format) {
+    public ConversionRequest(String inputPath, String outputPath, String format, String audioCodec, String videoCodec) {
         this.inputPath = inputPath;
         this.outputPath = outputPath;
         this.format = format;
+        this.audioCodec = audioCodec;
+        this.videoCodec = videoCodec;
     }
 
     public String getInputPath() {
@@ -36,5 +40,21 @@ public class ConversionRequest {
 
     public void setFormat(String format) {
         this.format = format;
+    }
+
+    public String getAudioCodec() {
+        return audioCodec;
+    }
+
+    public void setAudioCodec(String audioCodec) {
+        this.audioCodec = audioCodec;
+    }
+
+    public String getVideoCodec() {
+        return videoCodec;
+    }
+
+    public void setVideoCodec(String videoCodec) {
+        this.videoCodec = videoCodec;
     }
 }
