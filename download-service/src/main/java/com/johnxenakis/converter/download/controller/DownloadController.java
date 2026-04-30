56f6eb2b0d8c584a60713b31ce99efc7da93e443
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2026-04-30 17:59:59 +0300

    Implement download controller
    Implement DownloadController.java.
    
    Resolves: CU-86c8vcumn[complete]

---
+++ download-service/src/main/java/com/johnxenakis/converter/download/controller/DownloadController.java
@@ -0,0 +1,30 @@
+package com.johnxenakis.converter.download.controller;
+
+import com.johnxenakis.converter.download.service.DownloadService;
+import com.johnxenakis.converter.dto.JobPayload;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/download")
+public class DownloadController {
+
+    private final DownloadService downloadService;
+
+    public DownloadController (DownloadService downloadService) {
+        this.downloadService = downloadService;
+    }
+
+    @PostMapping
+    public ResponseEntity<InputStreamResource> download(@RequestBody JobPayload payload) {
+        return downloadService.download(
+                payload.getBlobName(),
+                payload.getMimeType(),
+                payload.getOutputFormat()
+        );
+    }
+}
