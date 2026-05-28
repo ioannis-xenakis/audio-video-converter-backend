2df6498ae664453ee306319fdd0b5e9096be810e
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2026-05-28 21:47:13 +0300

    Create new storage service
    Resolves: CU-86c4nayz3[complete]

---
+++ storage-service/src/main/java/com/johnxenakis/converter/storage/controller/StorageController.java
@@ -0,0 +1,16 @@
+package com.johnxenakis.converter.storage.controller;
+
+import com.johnxenakis.converter.storage.service.StorageService;
+import com.johnxenakis.converter.dto.JobPayload;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/store")
+public class StorageController {
+
+}
