4733d61060958046ce6ff95dfad168d8b912a709
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2026-09-07 19:32:12 +0300

    Add active profiles test in storage tests
    Add active profiles test code line in storage service tests.
    
    Resolves: CU-86cbbd6nb[complete]

--- storage-service/src/test/java/com/johnxenakis/converter/storage/StorageServiceApplicationTests.java
+++ storage-service/src/test/java/com/johnxenakis/converter/storage/StorageServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.johnxenakis.converter.storage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StorageServiceApplicationTests {
 
 	@Test
